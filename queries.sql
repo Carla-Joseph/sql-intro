b1874264a12942fc5207f001ab3ca1e68ac02c5b
Carla Joseph <carlajseph@gmail.com>
2020-05-26 14:13:53 -0400

    Explorer mode completed

---
+++ queries.sql
@@ -0,0 +1,36 @@
+CREATE TABLE "Employees" ("FullName" TEXT NOT NULL, "Salary" INT, "JobPosition" TEXT, "PhoneExtension" INT, "IsPartTime" TEXT);  
+
+INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "
+ PhoneExtension", "IsPartTime") Values ('Josh Smith', 82000, 'UX Researcher', 20
+ 9, 'No');    
+ 
+ INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension"
+ , "IsPartTime") Values ('Robyn Fenty', 750000, 'Performer', 456, 'Yes');
+ 
+ INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension"
+ , "IsPartTime") Values ('Jeff Bezos', 450000, 'CEO', 111, 'No');   
+ 
+ INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension"
+ , "IsPartTime") Values ('Jason Binder', 105000, 'Executive Chef', 654, 'No');   
+
+ INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension"
+ , "IsPartTime") Values ('Edward Conwell', 56000, 'Sous Chef', 610, 'No');  
+
+INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension"
+ , "IsPartTime") Values ('Miguel Dunkley', 72000, 'Webmaster', 598, 'Yes'); 
+
+ INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension"
+ , "IsPartTime") Values ('Lebron Louiscar', 86000, 'Engineer', 409, 'No'); 
+
+ INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension"
+ , "IsPartTime") Values ('Martine Johnson', 136000, 'Lead Designer', 156, 'No'); 
+
+ INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension"
+ , "IsPartTime") Values ('Ethan Hendrix', 280000, 'Vice President', 133, 'No');  
+
+ INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension"
+ , "IsPartTime") Values ('Aisha Carter', 94000, 'Consultant', 702, 'Yes');                     
+SELECT * FROM "Employees"; 
+
+SELECT "FullName", "PhoneExtension" FROM "Employees" WHERE "IsPartTime" = 'No'
+ ;                    
